83ee80d4b93fecad7ec2e38b4524f70087d089e1
giisen <kristerekelund@me.com>
2022-11-12 15:57:51 +0100

    Create KristerIntro.sql

---
+++ SQLÖvningarKrister/KristerIntro.sql
@@ -0,0 +1,23 @@
+--create database DjurDb;
+
+--create table DjurDb.dbo.DjurTbl(
+--Id Int Primary Key,
+--Namn nvarchar(max) not null);
+
+
+
+--INSERT INTO DjurDb.dbo.DjurTbl 
+--(Id,Namn)
+--VALUES
+--(1,'Hamster'),
+--(2,'Hund'),
+--(3,'Katt'),
+--(4,'Kanin');
+
+
+--select *
+--from DjurDb.dbo.DjurTbl
+--where Namn like '%a%';
+
+--drop table  DjurDb.dbo.DjurTbl;
+drop database DjurDb;
