ed9dd2323a805f544906acac0f599f8b5e8deff5
giisen <kristerekelund@me.com>
2022-11-13 15:07:17 +0100

    Update KristerIntro.sql
    Lagt till studieGrupp

--- SQLÖvningarKrister/KristerIntro.sql
+++ SQLÖvningarKrister/KristerIntro.sql
@@ -1,36 +1,49 @@
 --create database DjurDb;
---use DjurDb;
+--use DjurDb; 
 
 
 --create table DjurTbl(
---Id Int Primary Key,
+--Id Int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 --Namn nvarchar(max) not null);
 
-
-
 --INSERT INTO DjurTbl 
---(Id,Namn)
+--(Namn)
 --VALUES
---(1,'Hamster'),
---(2,'Hund'),
---(3,'Katt'),
---(4,'Kanin');
-
+--('Hamster'),
+--('Hund'),
+--('Katt'),
+--('Kanin');
 
 --select *
 --from DjurTbl
 --where Namn like '%a%';
 
---drop table  DjurTbl;
+--drop table DjurTbl;
 --use master;
 --drop database DjurDb;
+
+
+
 
 --create database XXX;
 -- use XXX;
 
 --create table StudieGrupp(
---Id int Primary Key,
+--Id Int NOT NULL IDENTITY(1,1) PRIMARY KEY,
 --Förnamn nvarchar(max) not null,
 --Efternamn nvarchar(max) not null,
 --Ålder int,
 --Hemort nvarchar(max) not null);
+
+--INSERT INTO StudieGrupp
+--(Förnamn, Efternamn,Ålder,Hemort)
+--VALUES
+--('Krister','Ekelund',45,'Torslanda'),
+--('X','XX',1,'XXX'),
+--('Y','YY',1,'YYY'),
+--('Z','ZZ',1,'ZZZ'),
+--('Förnamn','Efternamn',25,'Göteborg');
+
+--select *
+--from
+--StudieGrupp;
