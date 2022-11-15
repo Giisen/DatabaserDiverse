aee4ff990086c5be3246c42f509be25534e72196
giisen <kristerekelund@me.com>
2022-11-15 10:47:22 +0100

    Update ÖvningmedGruppen.sql

--- SQL_GruppÖvningar/ÖvningmedGruppen.sql
+++ SQL_GruppÖvningar/ÖvningmedGruppen.sql
@@ -1,5 +1,6 @@
 use Everyloop;
 
+--1.Ta ut data (select) från tabellen GameOfThrones på sådant sätt att ni får ut en kolumn ’Title’ med titeln samt en kolumn ’Episode’ som visar episoder och säsonger i formatet ”S01E01”, ”S01E02”, osv. Tips: kolla upp funktionen format()
 
 --select 
 --title, 
@@ -8,18 +9,20 @@ use Everyloop;
 --from GameOfThrones2;
 
 
+
+--2.Uppdatera (kopia på) tabellen user och sätt username för alla användare så den blir de 2 första bokstäverna i förnamnet, och de 2 första i efternamnet (istället för 3+3 som det är i orginalet). Hela användarnamnet ska vara i små bokstäver.
+
 --select * into Users2 from Users;
-
-
---Alter table Users2
---add UserName2 nvarchar(4);
-
 --update Users2
---set UserName2 =concat(substring(Lower(FirstName),1,2),substring(Lower(LastName),1,2));
+--set UserName =concat(substring(Lower(FirstName),1,2),substring(Lower(LastName),1,2));
 
 --select *
 --from
 --Users2;
+
+
+--3.Uppdatera (kopia på) tabellen airports så att alla null-värden i kolumnerna Time och DST byts ut mot ’-’
+
 
 --select * into Airports2 from Airports;
 
@@ -29,13 +32,9 @@ use Everyloop;
 
 --select * from Airports2;
 
---Ta bort de rader från (kopia på) tabellen Elements där ”Name” är någon av följande: 'Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium', samt alla rader där ”Name” börjar på någon av bokstäverna d, k, m, o, eller u.
+--4.Ta bort de rader från (kopia på) tabellen Elements där ”Name” är någon av följande: 'Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium', samt alla rader där ”Name” börjar på någon av bokstäverna d, k, m, o, eller u.
 
 --select * into Elements2 from Elements;
-
-
-
---select * from Elements2;
 
 --Delete from Elements2
 --where Name in('Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium')
@@ -45,6 +44,7 @@ use Everyloop;
 -- or Name like 'O%'
 -- or name like 'U%';
 
+--select * from Elements2;
 
 --5. Skapa en ny tabell med alla rader från tabellen Elements. Den nya tabellen ska innehålla ”Symbol” och ”Name” från orginalet, samt en tredje kolumn med värdet ’Yes’ för de rader där ”Name” börjar med bokstäverna i ”Symbol”, och ’No’ för de rader där de inte gör det.
 
@@ -79,3 +79,21 @@ use Everyloop;
 --left join Colors t2
 --on t1.Code=t2.Code;
 
+
+
+--7.Kopiera kolumnerna ”Integer” och ”String” från tabellen ”Types” till en ny tabell. Gör sedan en select från den nya tabellen som ger samma resultat som du skulle fått från select * from types;
+
+--select
+--[Integer],
+--String
+--into [Types2] from [Types];
+
+--select
+--t1.[Integer],
+--t1.string,
+--t2.[Float],
+--t2.[DateTime],
+--t2.[Bool]
+--from [Types2] t1
+--left join [Types] t2
+--on t1.[Integer]=t2.[Integer];
