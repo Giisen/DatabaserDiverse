d6fc872182109f40f9bcbd6b68087197c936c3f7
giisen <kristerekelund@me.com>
2022-11-15 08:18:30 +0100

    Create ÖvningmedGruppen.sql

---
+++ SQL_GruppÖvningar/ÖvningmedGruppen.sql
@@ -0,0 +1,15 @@
+use Everyloop;
+
+
+--select 
+--title, 
+--concat('S', format(season, '00'), 
+--'E', format(episode, '00')) as episode
+--from GameOfThrones2;
+
+
+--select * into Users2 from Users;
+
+select *
+from
+Users2;
