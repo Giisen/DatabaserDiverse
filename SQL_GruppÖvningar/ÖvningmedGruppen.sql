ab04577454afc251da173a7e74a19dc046c04533
giisen <kristerekelund@me.com>
2022-11-15 09:14:39 +0100

    Update ÖvningmedGruppen.sql

--- SQL_GruppÖvningar/ÖvningmedGruppen.sql
+++ SQL_GruppÖvningar/ÖvningmedGruppen.sql
@@ -10,6 +10,27 @@ use Everyloop;
 
 --select * into Users2 from Users;
 
-select *
-from
-Users2;
+
+--Alter table Users2
+--add UserName2 nvarchar(4);
+
+--update Users2
+--set UserName2 =concat(substring(Lower(FirstName),1,2),substring(Lower(LastName),1,2));
+
+--select *
+--from
+--Users2;
+
+--select * into Airports2 from Airports;
+
+--Update  Airports2
+----set [Time]= COALESCE([Time],'-')
+--set [DST] = COALESCE([DST],'-');
+
+--select * from Airports2;
+
+--Ta bort de rader från (kopia på) tabellen Elements där ”Name” är någon av följande: 'Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium', samt alla rader där ”Name” börjar på någon av bokstäverna d, k, m, o, eller u.
+
+
+
+
