1578b0d8780e823110b3dbaa3759b51ec31a0667
giisen <kristerekelund@me.com>
2022-11-23 11:56:25 +0100

    Update Övningar2.sql

--- SQL_GruppÖvningar/Övningar2.sql
+++ SQL_GruppÖvningar/Övningar2.sql
@@ -37,5 +37,40 @@ order by Antal desc;
 
 select * from company.orders
 select * from company.order_details
+select * from company.products
 
 --3.Av de produkter som inte längre finns I vårat sortiment, hur mycket har vi sålt för totalt till Tyskland?
+select 
+--t1.ProductID,
+sum(t1.Unitprice*t1.Quantity) as summa,
+t2.ShipCountry,
+t3.Discontinued
+from company.order_details t1
+
+join company.orders t2
+on t1.OrderId=t2.Id
+
+join company.products t3
+on t1.ProductId=t3.Id
+
+where t2.ShipCountry='Germany' and t3.Discontinued=1  
+group by
+--t1.ProductID,
+t2.ShipCountry,
+t3.Discontinued
+
+--4.För vilken produktkategori har vi högst lagervärde?
+select --top 1
+t1.CategoryName,
+Convert(int,sum(t2.Unitprice*t2.UnitsInStock)) as summa
+
+from 
+company.categories t1
+join
+company.products t2
+on t1.Id=t2.Id
+group by
+t1.CategoryName
+order by summa desc
+
+
